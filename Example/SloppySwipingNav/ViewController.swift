859e6c6eff26058acd6375bb636b928cdab59574
Yuki Mishima <mishimaybe@gmail.com>
2016-04-24 10:30:31 +0900

    Add source file

--- Example/SloppySwipingNav/ViewController.swift
+++ Example/SloppySwipingNav/ViewController.swift
@@ -1,24 +1,61 @@
-//
-//  ViewController.swift
-//  SloppySwipingNav
-//
-//  Created by Yuki Mishima on 04/23/2016.
-//  Copyright (c) 2016 Yuki Mishima. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        title = String(navigationController?.viewControllers.count ?? 0)
+
+        let colors: [UIColor] = [
+            UIColor(red: 251, green: 202, blue: 77),
+            UIColor(red: 163, green: 111, blue: 162),
+            UIColor(red: 236, green: 211, blue: 24),
+            UIColor(red: 244, green: 163, blue: 70),
+            UIColor(red: 78, green: 45, blue: 31),
+            UIColor(red: 133, green: 127, blue: 142),
+            UIColor(red: 15, green: 144, blue: 120),
+            UIColor(red: 193, green: 0, blue: 43),
+            UIColor(red: 145, green: 54, blue: 11),
+            UIColor(red: 168, green: 225, blue: 205),
+            UIColor(red: 239, green: 137, blue: 132),
+            UIColor(red: 58, green: 73, blue: 157),
+            UIColor(red: 216, green: 12, blue: 24),
+            UIColor(red: 0, green: 133, blue: 163),
+            UIColor(red: 233, green: 71, blue: 9),
+            UIColor(red: 211, green: 84, blue: 153),
+            UIColor(red: 227, green: 49, blue: 125),
+            UIColor(red: 228, green: 94, blue: 57),
+            UIColor(red: 250, green: 190, blue: 0),
+            UIColor(red: 125, green: 0, blue: 62),
+            UIColor(red: 0, green: 128, blue: 200),
+            UIColor(red: 165, green: 154, blue: 202),
+            UIColor(red: 244, green: 219, blue: 173),
+            UIColor(red: 0, green: 141, blue: 120),
+            UIColor(red: 183, green: 22, blue: 73),
+            UIColor(red: 244, green: 179, blue: 194),
+            UIColor(red: 248, green: 204, blue: 220),
+            UIColor(red: 182, green: 221, blue: 202),
+            UIColor(red: 157, green: 195, blue: 23),
+            UIColor(red: 237, green: 121, blue: 120),
+            UIColor(red: 255, green: 222, blue: 0),
+            UIColor(red: 199, green: 203, blue: 17),
+            UIColor(red: 148, green: 48, blue: 50),
+            UIColor(red: 136, green: 161, blue: 79),
+            UIColor(red: 239, green: 138, blue: 150),
+            UIColor(red: 202, green: 232, blue: 237),
+            UIColor(red: 0, green: 72, blue: 122),
+            UIColor(red: 0, green: 94, blue: 70),
+            UIColor(red: 246, green: 171, blue: 0),
+            UIColor(red: 202, green: 46, blue: 90),
+            ]
+        view.backgroundColor = colors[Int(arc4random_uniform(UInt32(colors.count)))]
     }
 
 }
 
+extension UIColor {
+    convenience init(red: UInt8, green: UInt8, blue: UInt8) {
+        self.init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: 1)
+    }
+}
---
+++ SloppySwipingNav/Classes/SloppySwipingNav.swift
@@ -0,0 +1,204 @@
+import UIKit
+
+class SloppySwipingNav: UINavigationController, UIViewControllerTransitioningDelegate, UINavigationControllerDelegate {
+
+    private class SloppySwipingPanGestureRecognizer: UIPanGestureRecognizer {
+    }
+
+    class AnimatedTransitioning: NSObject, UIViewControllerAnimatedTransitioning {
+
+        private let bottomViewOffset: CGFloat = -60
+
+        var reverse = false
+        var linerAnimation = false {
+            didSet {
+                duration = linerAnimation ? 0.4 : 0.4
+            }
+        }
+        private var duration: NSTimeInterval = 0
+
+        func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+            return duration
+        }
+
+        func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+            guard
+                let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey),
+                let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey) else {
+                    return
+            }
+            let finalFrameForToViewController = transitionContext.finalFrameForViewController(toViewController)
+            let containerView = transitionContext.containerView()
+
+            // 移動前のViewを設定
+            if !reverse {
+                toViewController.view.frame = CGRectOffset(finalFrameForToViewController, fromViewController.view.frame.width, 0)
+                addShadow(toViewController)
+                containerView?.addSubview(toViewController.view)
+            } else {
+                addShadow(fromViewController)
+                containerView?.insertSubview(toViewController.view, belowSubview: fromViewController.view)
+
+                toViewController.view.frame = CGRectOffset(finalFrameForToViewController, self.bottomViewOffset, 0)
+            }
+
+            // 移動させる
+            let animations = {
+                if !self.reverse {
+                    toViewController.view.frame = finalFrameForToViewController
+                    fromViewController.view.frame = CGRectOffset(finalFrameForToViewController, self.bottomViewOffset, 0)
+                } else {
+                    fromViewController.view.frame = CGRectOffset(finalFrameForToViewController, fromViewController.view.frame.width, 0)
+                    toViewController.view.frame = finalFrameForToViewController
+                }
+            }
+
+            // 移動後の処理
+            let completion = { (finished: Bool) in
+                if !self.reverse {
+                    self.removeShadow(toViewController)
+                    fromViewController.view.frame = finalFrameForToViewController
+                } else {
+                    self.removeShadow(fromViewController)
+                    fromViewController.view.frame.origin.x = 0
+                }
+
+                if transitionContext.transitionWasCancelled() {
+                    toViewController.view.removeFromSuperview()
+                } else {
+                    fromViewController.view.removeFromSuperview()
+                }
+                transitionContext.completeTransition(!transitionContext.transitionWasCancelled())
+            }
+
+            if linerAnimation {
+                UIView.animateWithDuration(
+                    transitionDuration(transitionContext),
+                    delay: 0,
+                    options: [.CurveLinear, .AllowUserInteraction],
+                    animations: animations,
+                    completion: completion
+                )
+            } else {
+                UIView.animateWithDuration(
+                    transitionDuration(transitionContext),
+                    delay: 0,
+                    usingSpringWithDamping: 1,
+                    initialSpringVelocity: 0,
+                    options: [.CurveEaseInOut, .AllowUserInteraction],
+                    animations: animations,
+                    completion: completion
+                )
+            }
+        }
+
+        private func addShadow(viewController: UIViewController) {
+            viewController.view.layer.masksToBounds = false
+            viewController.view.layer.shadowColor = UIColor.blackColor().CGColor
+            viewController.view.layer.shadowRadius = 5
+            viewController.view.layer.shadowOpacity = 0.2
+        }
+
+        private func removeShadow(viewController: UIViewController) {
+            viewController.view.layer.shadowRadius = 0
+        }
+
+    }
+
+    class InteractiveTransition: UIPercentDrivenInteractiveTransition, UIGestureRecognizerDelegate {
+
+        private var navigationController: UINavigationController?
+        private var shouldCompleteTransition = false
+        private(set) var transitionInProgress = false
+
+        func attachToViewController(viewController: UIViewController) {
+            navigationController = viewController.navigationController
+
+            let sloppySwipingGestures = viewController.view.gestureRecognizers?.filter { $0 is SloppySwipingPanGestureRecognizer }
+            if sloppySwipingGestures == nil || sloppySwipingGestures?.count == 0 {
+                let gestureRecognizer = SloppySwipingPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
+                gestureRecognizer.delegate = self
+                viewController.view.addGestureRecognizer(gestureRecognizer)
+            }
+        }
+
+        func handlePanGesture(gestureRecognizer: UIPanGestureRecognizer) {
+            switch gestureRecognizer.state {
+            case .Began:
+                transitionInProgress = true
+                navigationController?.popViewControllerAnimated(true)
+
+            case .Changed:
+                guard let view = gestureRecognizer.view else {
+                    break
+                }
+
+                let translation = gestureRecognizer.translationInView(view)
+                let percent: CGFloat = min(max(translation.x / view.frame.width, 0), 1)
+
+                let velocity = gestureRecognizer.velocityInView(view)
+                if velocity.x > 300 {
+                    shouldCompleteTransition = true
+                } else if velocity.x < -300 {
+                    shouldCompleteTransition = false
+                } else {
+                    shouldCompleteTransition = percent > 0.5
+                }
+
+                updateInteractiveTransition(percent)
+
+            case .Cancelled, .Ended:
+                transitionInProgress = false
+                if !shouldCompleteTransition || gestureRecognizer.state == .Cancelled {
+                    cancelInteractiveTransition()
+                } else {
+                    finishInteractiveTransition()
+                }
+
+            default:
+                break
+            }
+        }
+
+        func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer) -> Bool {
+            guard
+                let view = gestureRecognizer.view,
+                let gestureRecognizer = gestureRecognizer as? UIPanGestureRecognizer else {
+                    return false
+            }
+
+            let translation = gestureRecognizer.translationInView(view)
+            return fabs(translation.x) >= fabs(translation.y)
+        }
+
+    }
+
+    private let animatedTransitioning = AnimatedTransitioning()
+    private let interactiveTransition = InteractiveTransition()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        delegate = self
+    }
+
+    func navigationController(navigationController: UINavigationController, willShowViewController viewController: UIViewController, animated: Bool) {
+
+        if viewController != viewControllers.first { // Exclude root viewController
+            interactiveTransition.attachToViewController(viewController)
+        }
+    }
+
+    func navigationController(navigationController: UINavigationController, animationControllerForOperation operation: UINavigationControllerOperation, fromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+
+        animatedTransitioning.reverse = operation == .Pop
+        return animatedTransitioning
+    }
+
+    func navigationController(navigationController: UINavigationController, interactionControllerForAnimationController animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+
+        animatedTransitioning.linerAnimation = interactiveTransition.transitionInProgress
+        return interactiveTransition.transitionInProgress ? interactiveTransition : nil
+    }
+
+}
